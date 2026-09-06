9d53316b7b56dc0532d119531d22ccff28b3862b
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2026-09-06 19:07:32 +0300

    Make bookmark header follow system theme

--- components/ui/custom/Title.js
+++ components/ui/custom/Title.js
@@ -1,9 +1,9 @@
 // components/ui/custom/BookmarkHeader.jsx
 export default function BookmarkHeader() {
-    return (
-      <div className="border-b font-semibold text-[16px] flex items-center gap-1 p-2 fixed top-0 bg-white w-full">
-        <img src="/Rectangle 2.png" className="w-3 h-4" />
-        <span>Bookmark</span>
-      </div>
-    )
-  }
+  return (
+    <div className="border-b font-semibold text-[16px] flex items-center gap-1 p-2 fixed top-0 bg-background text-foreground w-full z-40">
+      <img src="/Rectangle 2.png" className="w-3 h-4" />
+      <span>Bookmark</span>
+    </div>
+  )
+}
