584b78f2d6f32ed5709d4fb95eaecea5989c4ea7
İsmail Caner <126804265+ismailcaner@users.noreply.github.com>
2026-09-06 19:37:52 +0300

    Set dropdown menu dark background to #181818

--- components/ui/custom/BookmarkItem.js
+++ components/ui/custom/BookmarkItem.js
@@ -104,7 +104,7 @@ export default function BookmarkItem({ item, selectMode, selected, onLongPress, 
                 <Menu color={item.pinned ? "#ff5d26" : "grey"} />
               </DropdownMenuTrigger>
 
-              <DropdownMenuContent align="end" className="flex flex-col gap-1 bg-zinc-50">
+              <DropdownMenuContent align="end" className="flex flex-col gap-1 bg-zinc-50 dark:bg-[#181818]">
                 <DropdownMenuItem
                   onClick={() => togglePin(item)}
                   className="p-2.5 rounded-br-sm rounded-bl-sm h-fit text-zinc-500 bg-zinc-100 border-0 font-semibold grid grid-cols-[20px_1fr] dark:bg-[#2a2a2a]"
